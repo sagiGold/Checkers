c813ae5fa9bbfb863c663224f43b657972185e0d
sagi goldfarb <84674218+sagiGold@users.noreply.github.com>
2022-04-19 16:16:18 +0300

    working on Board

--- Checkers/Board.cs
+++ Checkers/Board.cs
@@ -5,7 +5,64 @@ using System.Text;
 
 namespace Checkers
 {
-    class Board
+    public class Board
     {
+        public enum eBoradSize
+        {
+            Small = 6,
+            Medium = 8,
+            Large = 10
+        }
+
+        private readonly char[,] m_GameBoard;
+        private int m_Height;
+        private int m_Width;
+
+        public Board(int height, int width)
+        {
+            m_Height = height;
+            m_Width = width;
+            m_GameBoard = new char[height, width];
+        }
+
+        public char this[int i_Row, int i_Colum]
+        {
+            get
+            {
+                return m_GameBoard[i_Row, i_Colum];
+            }
+
+            set
+            {
+                m_GameBoard[i_Row, i_Colum] = value;
+            }
+        }
+
+        public int Height
+        {
+            get
+            {
+                return m_Height;
+            }
+
+            set
+            {
+                m_Height = value;
+            }
+        }
+
+        public int Width
+        {
+            get
+            {
+                return m_Width;
+            }
+
+            set
+            {
+                m_Width = value;
+            }
+        }
+
     }
 }
